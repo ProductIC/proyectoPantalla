3774f08022360694f4ebfd8eafdfa62387ed06b1
ProductIC <santanaiancillero@gmail.com>
2022-02-17 08:29:59 +0000

    GestionNoticias a mitad

--- src/main/java/Datos/GestionNoticias.java
+++ src/main/java/Datos/GestionNoticias.java
@@ -5,10 +5,33 @@
  */
 package Datos;
 
+import Conexión.MyConnection;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  *
  * @author AlumnadoManana
  */
 public class GestionNoticias {
+    private Connection connection;
     
+    public GestionNoticias(){
+        MyConnection miCon;
+        try{
+             miCon=new MyConnection();
+            connection=miCon.getCon();
+            
+        }catch(ClassNotFoundException ex){
+        System.out.println("Error: con la base de datos"+ex);
+        }
+    }
+    public boolean insertNoticia(Noticia noti){
+        try{
+            PreparedStatement stmt= connection.prepareStatement("INSERT into noticias(url,fecha,duracion)VALUES(?,?,?)");
+        }catch(SQLException ex){
+            System.out.println("Error al insertar noticia");
+            return false;
+        }
+    }
 }
