e9c54f781c818b2b27550be9207115f61cbd3b19
ProductIC <santanaiancillero@gmail.com>
2022-02-17 08:48:29 +0000

    GestionNoticias hecha

--- src/main/java/Datos/GestionNoticias.java
+++ src/main/java/Datos/GestionNoticias.java
@@ -7,6 +7,7 @@ package Datos;
 
 import Conexión.MyConnection;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 /**
@@ -29,6 +30,15 @@ public class GestionNoticias {
     public boolean insertNoticia(Noticia noti){
         try{
             PreparedStatement stmt= connection.prepareStatement("INSERT into noticias(url,fecha,duracion)VALUES(?,?,?)");
+            stmt.setString(1,noti.getUrl());
+            java.util.Date utilStartDate = noti.getFecha();
+            java.sql.Date sqlStartDate = new java.sql.Date(utilStartDate.getTime());
+            stmt.setDate(2,sqlStartDate);
+            java.util.Date fechafin = noti.getDuracion();
+            java.sql.Date sqlfechafinDate = new java.sql.Date(fechafin.getTime());
+            stmt.setDate(3, sqlfechafinDate);
+            stmt.executeUpdate();
+            return true;
         }catch(SQLException ex){
             System.out.println("Error al insertar noticia");
             return false;
